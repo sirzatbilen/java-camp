7274fc14bdd74ed849b5bc9cd42148c8daf0f0d1
şirzat bilen <93521133+sirzatbilen@users.noreply.github.com>
2022-09-28 04:25:53 +0300

    Add files via upload

---
+++ variables/variables/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 16;
+        String mesaj ="öğrenci sayısı : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+
+
+    }
+}
